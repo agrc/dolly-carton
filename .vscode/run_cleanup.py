19636e91aea7761ca3363609b5ca35e9cfa93338
stdavis <stdavis@utah.gov>
2025-07-25 19:22:23 +0000

    chore: add launch configurations and wrapper scripts for debugging main and cleanup functions

---
+++ .vscode/run_cleanup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Wrapper script to run the cleanup_dev_agol_items function for debugging."""
+
+if __name__ == "__main__":
+    from dolly.main import cleanup_dev_agol_items
+    cleanup_dev_agol_items()
