7394dc2390fa50f1534e15042bf8ecc94f0b520e
Scott Davis <stdavis@utah.gov>
2025-10-07 08:10:24 -0600

    feat: implement python-dotenv
    To make this easier to run on a server

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         "google-cloud-logging==3.*",
         "humanize==4.*",
         "pyodbc==5.*",
+        "python-dotenv==1.*",
         "requests==2.*",
         "ugrc-supervisor==3.*",
         "typer==0.*",
